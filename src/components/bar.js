4191550d96a505c7e2fb4f0d0903be9a3fdd5093
Musfarr <musfarhassan@gmail.com>
2023-07-13 17:20:05 +0500

    Added Convex Logo

--- src/components/bar.js
+++ src/components/bar.js
@@ -6,12 +6,12 @@ const Bar = () => {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center flex-shrink-0">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
+            src="https://www.convexinteractive.com/images/logoblack.svg"
             className="h-8 mr-3"
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Convex
+            
           </span>
         </a>
         <button
--- src/components/footer.js
+++ src/components/footer.js
@@ -15,8 +15,8 @@ const Foot = () => {
         <div className ="md:flex md:justify-between">
           <div className ="mb-6 md:mb-0">
               <a href="https://flowbite.com/" className ="flex items-center">
-                  <img src="https://flowbite.com/docs/images/logo.svg" className ="h-8 mr-3" alt="FlowBite Logo" />
-                  <span className ="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                  <img src="https://www.convexinteractive.com/images/logoblack.svg" className ="h-8 mr-3" alt="FlowBite Logo" />
+                  <span className ="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
               </a>
           </div>
           <div className ="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
